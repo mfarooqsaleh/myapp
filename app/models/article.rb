0fe9f5934eaa719a2e3f0086995c0bd8d997b43f
myapp <mfarooqsaleh@gmail.com>
2016-08-16 17:34:00 +0500

    created the article user interface

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true, length:{minmum: 3 ,maximun :30}
-    validates :description, presence: true, length:{minmum: 3 ,maximun :300}
+	#validates :title, presence: true, length:{minmum: 3 ,maximun :30}
+    #validates :description, presence: true, length:{minmum: 3 ,maximun :300}
 end
