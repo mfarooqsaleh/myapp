250b9e5028a51562399c2adce69b91b93b9879d2
myapp <mfarooqsaleh@gmail.com>
2017-01-11 22:37:32 +0500

    redundencies reduce

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:edit, :update,:show,:destroy]
+
 def index
 @articles = Article.all
 
@@ -7,7 +9,6 @@ def new
 	@article= Article.new
 end
 def edit
-	@article =Article.find(params[:id])
 	
 
 end
@@ -23,15 +24,12 @@ def create
 end
 end
 def show
-@article=Article.find(params[:id])
 end
 def destroy
-@article=Article.find(params[:id])
 flash[:notice]="article is deleted"
 redirect_to articles_path
 end
 def update
-@article = Article.find(params[:id])
 	if @article.update(article_params)
 	flash[:notice]= "Article is successfuly created"
 	redirect_to article_path(@article)	
@@ -44,6 +42,10 @@ end
 
 
 private
+def set_article
+	@article=Article.find(params[:id])
+end
+
   def article_params
   	params.require(:article).permit(:title,:description)
   end
