d2cc78f2a320aa8d1f954db0f26c14945cc077b9
myapp <mfarooqsaleh@gmail.com>
2017-01-24 21:50:59 +0500

    DEBUGGER

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ end
 def create
 	#render plain: params[:article].inspect
 	@article=Article.new(article_params)
+	@article.user=User.first
 	if @article.save
 		flash[:success]= "Article was succesfullay created"
 	    redirect_to article_path(@article)
