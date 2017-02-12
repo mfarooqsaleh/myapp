b210e5063a044c64aa74b74cafd5d30aa37b0357
myapp <mfarooqsaleh@gmail.com>
2017-02-12 16:32:42 +0500

    complete verification

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 before_action :set_article, only: [:edit, :update,:show,:destroy]
-
+before_action :require_user, except:[:index, :show]
+before_action :require_same_user, only:[:edit ,:update ,:destroy]
 def index
 @articles = Article.paginate(page: params[:page], per_page: 5)
 
@@ -18,7 +19,7 @@ end
 def create
 	#render plain: params[:article].inspect
 	@article=Article.new(article_params)
-	@article.user=User.first
+	@article.user=current_user
 	if @article.save
 		flash[:success]= "Article was succesfullay created"
 	    redirect_to article_path(@article)
@@ -52,5 +53,13 @@ end
 
   def article_params
   	params.require(:article).permit(:title,:description)
-  end
+ end
+ def require_same_user
+  	if current_user !=@article.user
+  		flash[:danger]= "You can only edit or delete your own articles"
+  		redirect_to root_path
+	end
+
+ end
+
 end
